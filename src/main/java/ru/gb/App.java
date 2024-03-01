5da81982ecbe6076e61b5071a3b4db0ba1cb8052
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-02 01:22:30 +0300

    Упростил pom.xml, добавил плагины для сборки в один jar с зависимостями

--- src/main/java/ru/gb/App.java
+++ src/main/java/ru/gb/App.java
@@ -1,5 +1,8 @@
 package ru.gb;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+
 /**
  * Hello world!
  *
@@ -9,6 +12,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] inputArray = new int[] {1,2,3,4,5};
+        
+        // Get a DescriptiveStatistics instance
+        DescriptiveStatistics stats = new DescriptiveStatistics();
+
+        // Add the data from the array
+        for( int i = 0; i < inputArray.length; i++) {
+                stats.addValue(inputArray[i]);
+        }
+
+        // Compute some statistics
+        double mean = stats.getMean();
+        double std = stats.getStandardDeviation();
+        double median = stats.getPercentile(50);
+
+        System.out.println(mean + " " + std + " " + median);
+
+        System.out.println("Привет!");
     }
 }
